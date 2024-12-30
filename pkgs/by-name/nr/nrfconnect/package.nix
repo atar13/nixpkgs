8a8ef537135bf07713d1645dc1090792c5331764
Anthony Tarbinian <atar137h@gmail.com>
2024-12-29 21:25:11 -0800

    nrfconnect 5.1.0

--- pkgs/by-name/nr/nrfconnect/package.nix
+++ pkgs/by-name/nr/nrfconnect/package.nix
@@ -5,7 +5,7 @@
 
 let
   pname = "nrfconnect";
-  version = "5.0.2";
+  version = "5.1.0";
 
   src = fetchurl {
     url = "https://nsscprodmedia.blob.core.windows.net/prod/software-and-other-downloads/desktop-software/nrf-connect-for-desktop/${lib.versions.major version}-${lib.versions.minor version}-${lib.versions.patch version}/nrfconnect-${version}-x86_64.appimage";
